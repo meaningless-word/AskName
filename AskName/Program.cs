38be9cc8f174e064a5b95917c8e317bcd990b5c0
Роман Толмачёв <unifiq@gmail.com>
2020-12-17 09:45:27 +0700

    Добавьте файлы проекта.

---
+++ AskName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AskName
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Roman";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
