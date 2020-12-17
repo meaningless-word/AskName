a115ad6bf2d2b82b6eba45eb331052eb8f6a184e
Роман Толмачёв <unifiq@gmail.com>
2020-12-17 10:08:05 +0700

    литералы

--- AskName/Program.cs
+++ AskName/Program.cs
@@ -9,6 +9,16 @@ namespace AskName
             string myName = "Roman";
 
             Console.WriteLine(myName);
+            // символьные литералы
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", myName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\x23");
+
+            //логические литералы
+            Console.WriteLine(true);
+            Console.WriteLine(false);
 
             Console.ReadKey();
         }
